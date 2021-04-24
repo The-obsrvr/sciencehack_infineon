normalization fixed

src/load_data.py
@@ -4,12 +4,11 @@ import torch
 import h5py
 import albumentations as A
 from torch.utils.data import Dataset, DataLoader
-import torchvision.transforms as F
+import torchvision.transforms.functional as F
 
 def toTensor(img, **params):
     return F.to_tensor(img)
-def normalize(img, **params):
-    return F.to_tensor(img)
+
 
 class CustomDataset(Dataset):
 
@@ -17,8 +16,8 @@ class CustomDataset(Dataset):
 
         self.data = h5py.File(path, 'r')
         self.split = split
-        # self.transform = A.Compose([A.Normalize([0.5], [0.5]),A.Lambda(p=1, image=toTensor)])
-        self.transform = F.Compose([F.ToTensor(), F.Normalize([385], [2691.76])])
+        self.transform = A.Compose([A.Normalize([385], [2691.76]), A.Lambda(p=1, image=toTensor)])
+        # self.transform = F.Compose([F.ToTensor(), F.Normalize([385], [2691.76])])
 
     def __getitem__(self, idx):
         """
@@ -63,8 +62,8 @@ class CustomDataset(Dataset):
         target["image_id"] = img_id
         target["area"] = area
         target["iscrowd"] = iscrowd
-        transformed = self.transform(img)
-        img = transformed#["image"]
+        transformed = self.transform(image=img)
+        img = transformed["image"]
         return img, target
 
     def __len__(self):